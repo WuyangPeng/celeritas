0b31559af4c2c313305be7d14776fc94a5f8af69
彭武阳 <94458936@qq.com>
2025-09-17 20:34:45 +0800

    + 还原

--- bat/compile.sh
+++ bat/compile.sh
@@ -3,9 +3,11 @@ cd ..
 mkdir -p build-debug build-release
 
 cd build-debug
+cmake ..
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make
 
 cd ../build-release
+cmake ..
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make 
