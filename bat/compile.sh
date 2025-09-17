e7781ad1a7c96a234a60f4b9a0b673097a7271b3
彭武阳 <94458936@qq.com>
2025-09-17 20:30:16 +0800

    + 先clear再编译

--- bat/compile.sh
+++ bat/compile.sh
@@ -3,9 +3,11 @@ cd ..
 mkdir -p build-debug build-release
 
 cd build-debug
+cmake clear
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make
 
 cd ../build-release
+cmake clear
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make 
