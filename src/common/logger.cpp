49d9d9be375fb848338e5c5e518f78e1094396dc
彭武阳 <94458936@qq.com>
2025-09-17 21:01:53 +0800

    + 日志库修改

--- src/common/logger.cpp
+++ src/common/logger.cpp
@@ -2,7 +2,6 @@
 
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/log/expressions/formatters/date_time.hpp>
-#include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
@@ -10,39 +9,50 @@
 #include <boost/log/sinks/text_ostream_backend.hpp>
 #include <boost/log/utility/setup/console.hpp>
 
-boost::log::sources::severity_logger<celeritas::severity_level> celeritas::logger::logger_;
-
-void celeritas::logger::init(const std::string& log_file_name)
+auto get_formatter()
 {
     // 设置日志格式
-    const auto formatter = boost::log::expressions::stream
-                           << "["
-                           << boost::log::expressions::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S.%f")
-                           << "]"
-                           << " ["
-                           << boost::log::trivial::severity
-                           << "] "
-                           << boost::log::expressions::smessage;
+    return boost::log::expressions::stream
+           << "["
+           << boost::log::expressions::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S.%f")
+           << "]"
+           << " ["
+           << boost::log::trivial::severity
+           << "] "
+           << boost::log::expressions::smessage;
+}
 
-    // 添加控制台日志输出
-    const auto console_sink = boost::log::add_console_log(std::clog);
-    console_sink->set_formatter(formatter);
-
+void celeritas::logger::init(const std::string& log_file_name, severity_level_type file_level)
+{
     // 添加文件日志输出
     boost::log::add_file_log(
         boost::log::keywords::file_name = log_file_name,
         boost::log::keywords::auto_flush = true,
-        boost::log::keywords::rotation_size = 10 * 1024 * 1024 // 10MB
-        )->set_formatter(formatter);
+        boost::log::keywords::rotation_size = 10 * 1024 * 1024, // 10MB
+        boost::log::keywords::filter = boost::log::trivial::severity >= file_level
+        )->set_formatter(get_formatter());
+}
 
+void celeritas::logger::init_global(severity_level_type level)
+{
     // 添加通用属性，如时间戳
     boost::log::add_common_attributes();
 
     // 设置全局日志级别
-    boost::log::core::get()->set_filter(boost::log::trivial::severity >= boost::log::trivial::info);
+    boost::log::core::get()->set_filter(boost::log::trivial::severity >= level);
 }
 
-boost::log::sources::severity_logger<celeritas::severity_level>& celeritas::logger::get()
+void celeritas::logger::init_console(severity_level_type console_level)
 {
-    return logger_;
+    // 添加控制台日志输出
+    const auto console_sink = boost::log::add_console_log(std::clog);
+    console_sink->set_formatter(get_formatter());
+    console_sink->set_filter(boost::log::trivial::severity >= console_level);
+}
+
+celeritas::logger::severity_logger_type& celeritas::logger::get()
+{
+    static severity_logger_type logger{};
+
+    return logger;
 }
--- src/common/logger.h
+++ src/common/logger.h
@@ -1,37 +1,28 @@
 #pragma once
 
+#include <boost/log/trivial.hpp>
 #include <boost/log/sources/severity_logger.hpp>
 #include <boost/log/sources/record_ostream.hpp>
 
 namespace celeritas
 {
-    // 定义日志级别
-    enum severity_level
-    {
-        debug,
-        info,
-        warning,
-        error,
-        fatal
-    };
-
     class logger
     {
     public:
         using class_type = logger;
-        using severity_logger_type = boost::log::sources::severity_logger<severity_level>;
+        using severity_level_type = boost::log::trivial::severity_level;
+        using severity_logger_type = boost::log::sources::severity_logger<severity_level_type>;
 
         // 初始化日志系统
-        static void init(const std::string& log_file_name);
+        static void init_global(severity_level_type level);
+        static void init_console(severity_level_type console_level);
+        static void init(const std::string& log_file_name, severity_level_type file_level);
 
         // 获取日志实例
         [[nodiscard]] static severity_logger_type& get();
-
-    private:
-        static severity_logger_type logger_;
     };
 }
 
 // 全局日志对象
 // 在你的代码中，使用 LOG(severity_level) << "你的日志信息" 来记录
-#define LOG(level) BOOST_LOG_SEV(logger::get(), level)
+#define LOG(level) BOOST_LOG_SEV(logger::get(), boost::log::trivial::severity_level::level)
