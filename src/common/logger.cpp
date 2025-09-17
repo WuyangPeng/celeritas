bc0cfddb838a44651a14e2a680a0b4de21873023
彭武阳 <94458936@qq.com>
2025-09-17 18:11:50 +0800

    + 日志库修改

--- src/common/logger.cpp
+++ src/common/logger.cpp
@@ -7,21 +7,22 @@
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/support/date_time.hpp>
+#include <boost/log/sinks/text_ostream_backend.hpp>
+#include <boost/log/utility/setup/console.hpp>
 
-// 初始化静态成员变量
 boost::log::sources::severity_logger<celeritas::severity_level> celeritas::logger::logger_;
 
 void celeritas::logger::init(const std::string& log_file_name)
 {
     // 设置日志格式
-    const boost::log::formatter formatter = boost::log::expressions::stream
-        << "["
-        << boost::log::expressions::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S.%f")
-        << "]"
-        << " ["
-        << boost::log::trivial::severity
-        << "] "
-        << boost::log::expressions::smessage;
+    const auto formatter = boost::log::expressions::stream
+                           << "["
+                           << boost::log::expressions::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S.%f")
+                           << "]"
+                           << " ["
+                           << boost::log::trivial::severity
+                           << "] "
+                           << boost::log::expressions::smessage;
 
     // 添加控制台日志输出
     const auto console_sink = boost::log::add_console_log(std::clog);
@@ -32,7 +33,7 @@ void celeritas::logger::init(const std::string& log_file_name)
         boost::log::keywords::file_name = log_file_name,
         boost::log::keywords::auto_flush = true,
         boost::log::keywords::rotation_size = 10 * 1024 * 1024 // 10MB
-    )->set_formatter(formatter);
+        )->set_formatter(formatter);
 
     // 添加通用属性，如时间戳
     boost::log::add_common_attributes();
--- src/common/logger.h
+++ src/common/logger.h
@@ -1,15 +1,6 @@
 #pragma once
 
-#include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/sinks/text_ostream_backend.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/console.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
 
 namespace celeritas
 {
@@ -25,7 +16,7 @@ namespace celeritas
 
     // 全局日志对象
     // 在你的代码中，使用 LOG(severity_level) << "你的日志信息" 来记录
-#define LOG(level) BOOST_LOG_SEV(Logger::get(), level)
+    #define LOG(level) BOOST_LOG_SEV(Logger::get(), level)
 
     class logger
     {
