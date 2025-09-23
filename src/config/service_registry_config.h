a63cb4b78c337c2593866b59cfe54edbb6760921
彭武阳 <94458936@qq.com>
2025-09-23 22:18:41 +0800

    + 配置

--- src/config/service_registry_config.h
+++ src/config/service_registry_config.h
@@ -1,11 +1,18 @@
 ﻿#pragma once
 
+#include <string>
+
 namespace celeritas
 {
     class service_registry_config
     {
     public:
         using class_type = service_registry_config;
+
+    private:
+        std::string ipc_name;
+        std::string host;
+        int port = 0;
     };
 }
 
