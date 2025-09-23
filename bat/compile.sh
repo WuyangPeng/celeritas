b8eb642c6fd8c9bcb2dd51fa5f727e93aa41fbb7
彭武阳 <94458936@qq.com>
2025-09-23 15:05:39 +0800

    + cmake文件修改

--- bat/compile.sh
+++ bat/compile.sh
@@ -5,11 +5,11 @@ mkdir -p build-debug build-release
 cd build-debug
 cmake ..
 cmake -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build . --parallel
+make
 cmake --install . 
 
 cd ../build-release
 cmake ..
 cmake -DCMAKE_BUILD_TYPE=Release ..
-cmake --build . --parallel
+make
 cmake --install . 
