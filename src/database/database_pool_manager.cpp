11d506e4c4d9e5ffa9d8cca3dc08699d7c93a411
彭武阳 <94458936@qq.com>
2025-09-26 14:46:19 +0800

    加载数据库

--- src/database/database_pool_manager.cpp
+++ src/database/database_pool_manager.cpp
@@ -1,1 +1,63 @@
-﻿#include "common/database_pool_manager.h"
+﻿#include "database_pool_manager.h"
+#include "common/celeritas_error.h"
+
+celeritas::database_pool_manager& celeritas::database_pool_manager::get_instance()
+{
+    static database_pool_manager manager{};
+
+    return manager;
+}
+
+celeritas::database_pool_manager::database_pool_shared_ptr celeritas::database_pool_manager::create_pool(const std::string& key,
+                                                                                                         database_type database_type,
+                                                                                                         boost::asio::io_context& io_context,
+                                                                                                         const std::string& host,
+                                                                                                         uint16_t port,
+                                                                                                         const std::string& user,
+                                                                                                         const std::string& password,
+                                                                                                         const std::string& db_name,
+                                                                                                         size_t pool_size)
+{
+    switch (database_type)
+    {
+        case database_type::mysql:
+        {
+            return create_mysql_pool(key, io_context, host, port, user, password, db_name, pool_size);
+        }
+        default:
+        {
+            throw new celeritas_error("create pool ,key = " + key + ",database_type =" + std::to_string(static_cast<int>(database_type)) + " is  not exist.");
+        }
+    }
+}
+
+celeritas::database_pool_manager::database_pool_shared_ptr celeritas::database_pool_manager::get_pool(const std::string& key)
+{
+    std::lock_guard lock{ mutex_ };
+
+    if (const auto pool = pools_.find(key);
+        pool != pools_.cend())
+    {
+        return pool->second;
+    }
+
+    throw new celeritas_error("get pool ,key = " + key + " is  not exist.");
+}
+
+celeritas::database_pool_manager::database_pool_shared_ptr celeritas::database_pool_manager::create_mysql_pool(const std::string& key,
+                                                                                                               boost::asio::io_context& io_context,
+                                                                                                               const std::string& host,
+                                                                                                               uint16_t port,
+                                                                                                               const std::string& user,
+                                                                                                               const std::string& password,
+                                                                                                               const std::string& db_name,
+                                                                                                               size_t pool_size)
+{
+    std::lock_guard lock{ mutex_ };
+
+    database_pool_shared_ptr pool = std::make_shared<mysql_database_pool>(io_context, host, port, user, password, db_name, pool_size);
+
+    pools_.insert({ key, pool });
+
+    return pool;
+}
--- src/database/database_pool_manager.h
+++ src/database/database_pool_manager.h
@@ -1,5 +1,9 @@
 ﻿#pragma once
 
+#include "mysql_database_pool.h"
+#include "config/database_type.h"
+
+#include <map>
 
 namespace celeritas
 {
@@ -7,5 +11,38 @@ namespace celeritas
     {
     public:
         using class_type = database_pool_manager;
+
+        using database_pool_shared_ptr = std::shared_ptr<database_pool>;
+
+        [[nodiscard]] static database_pool_manager& get_instance();
+
+        [[nodiscard]] database_pool_shared_ptr create_pool(const std::string& key,
+                                                           database_type database_type,
+                                                           boost::asio::io_context& io_context,
+                                                           const std::string& host,
+                                                           uint16_t port,
+                                                           const std::string& user,
+                                                           const std::string& password,
+                                                           const std::string& db_name,
+                                                           size_t pool_size);
+
+        [[nodiscard]] database_pool_shared_ptr get_pool(const std::string& key);
+
+    private:
+        using database_pool_container = std::map<std::string, database_pool_shared_ptr>;
+
+        database_pool_manager() noexcept = default;
+
+        [[nodiscard]] database_pool_shared_ptr create_mysql_pool(const std::string& key,
+                                                                 boost::asio::io_context& io_context,
+                                                                 const std::string& host,
+                                                                 uint16_t port,
+                                                                 const std::string& user,
+                                                                 const std::string& password,
+                                                                 const std::string& db_name,
+                                                                 size_t pool_size);
+
+        database_pool_container pools_;
+        std::mutex mutex_;
     };
 }
