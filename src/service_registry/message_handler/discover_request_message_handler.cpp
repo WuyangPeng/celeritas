7f14e96c4b75ecec38701261be8fb518691b4686
彭武阳 <94458936@qq.com>
2025-10-14 17:57:04 +0800

    + 数据库

--- src/service_registry/message_handler/discover_request_message_handler.cpp
+++ src/service_registry/message_handler/discover_request_message_handler.cpp
@@ -1,7 +1,10 @@
 ﻿#include "discover_request_message_handler.h"
 #include "message/concrete_message_handler.tpp"
+#include "service_registry/service_registry.h"
 
 bool celeritas::discover_request_message_handler::handle_concrete(const header& header, const message_shared_ptr& message, const message_registry_weak_ptr& message_registry)
 {
+    const auto services = service_registry::get_services(message->service_name());
+
     return true;
 }
--- src/service_registry/message_handler/register_request_message_handler.cpp
+++ src/service_registry/message_handler/register_request_message_handler.cpp
@@ -1,7 +1,11 @@
 ﻿#include "register_request_message_handler.h"
 #include "message/concrete_message_handler.tpp"
+#include "service_registry/service_registry.h"
 
 bool celeritas::register_request_message_handler::handle_concrete(const header& header, const message_shared_ptr& message, const message_registry_weak_ptr& message_registry)
 {
+    const service_info service_info{ message->instance_id(), message->service_name(), message->host(), message->port(), message->game_server_id() };
+    service_registry::register_service(service_info);
+
     return true;
 }
