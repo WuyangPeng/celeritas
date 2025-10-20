da38d8e46ad45fed8c5a6036b193ef6e3dd32f66
彭曄恩 <94458936@qq.com>
2025-10-20 21:37:21 +0800

    + 服务器

--- bat/create_lib.sh
+++ bat/create_lib.sh
@@ -16,17 +16,17 @@ cd ${DEPS_DIR}
 version="1.88.0" 
 boost_dir="boost_1_88_0" 
 
-if [ ! -f /data/celeritas/deps/boost_installed.txt ]; then
+if [ ! -f ${DEPS_DIR}/boost_installed.txt ]; then
 
 	if [ ! -f ${boost_dir}.tar.gz ]; then
 		wget https://archives.boost.io/release/${version}/source/${boost_dir}.tar.gz
 	fi 
 	
-	if [ ! -f /data/celeritas/deps/boost_download.txt ]; then
+	if [ ! -f ${DEPS_DIR}/boost_download.txt ]; then
 		tar -zxvf ${boost_dir}.tar.gz
 		rm -rf boost
 		mv ${boost_dir} boost
-		touch /data/celeritas/deps/boost_download.txt
+		touch ${DEPS_DIR}/boost_download.txt
 	fi 
 	
 	cd boost
@@ -35,17 +35,17 @@ if [ ! -f /data/celeritas/deps/boost_installed.txt ]; then
 		./bootstrap.sh --with-libraries=all --with-toolset=gcc
 	fi 
 	
-	./b2 install --prefix=/data/celeritas/deps/boost/stage/ cxxflags="-std=c++20"
-	touch /data/celeritas/deps/boost_installed.txt
+	./b2 install --prefix=${DEPS_DIR}/boost/stage/ cxxflags="-std=c++20"
+	touch ${DEPS_DIR}/boost_installed.txt
 	
 fi 
 
 #! 编译hiredis库
-if [ ! -f /data/celeritas/Libs/hiredis_installed.txt ]; then
+if [ ! -f ${DEPS_DIR}/hiredis_installed.txt ]; then
 
-    cd /data/celeritas/deps/
+    cd ${DEPS_DIR}/
 	
-	if [ ! -f /data/celeritas/deps/hiredis_clone.txt ]; then
+	if [ ! -f ${DEPS_DIR}/hiredis_clone.txt ]; then
 	
 		rm -rf hiredis
 
@@ -56,15 +56,15 @@ if [ ! -f /data/celeritas/Libs/hiredis_installed.txt ]; then
 			cd hiredis
             git checkout v1.3.0
 	 
-			touch /data/celeritas/deps/hiredis_clone.txt
+			touch ${DEPS_DIR}/hiredis_clone.txt
 	
 		fi
 	
 	fi 
 	
-	if [ -f /data/celeritas/deps/hiredis_clone.txt ]; then
+	if [ -f ${DEPS_DIR}/hiredis_clone.txt ]; then
 	
-		cd /data/celeritas/deps/hiredis
+		cd ${DEPS_DIR}/hiredis
 	
 		mkdir -p build
 		cd build
@@ -74,7 +74,7 @@ if [ ! -f /data/celeritas/Libs/hiredis_installed.txt ]; then
 		cmake ../.. -DCMAKE_BUILD_TYPE=Debug
 		make 
 		
-		cd /data/celeritas/deps/hiredis/build
+		cd ${DEPS_DIR}/hiredis/build
 		mkdir -p release
 		cd release
 
@@ -83,7 +83,7 @@ if [ ! -f /data/celeritas/Libs/hiredis_installed.txt ]; then
 	 
 		if [ $? -eq 0 ]; then 
 	 
-			touch /data/celeritas/deps/hiredis_installed.txt
+			touch ${DEPS_DIR}/hiredis_installed.txt
 	
 		fi
 	
@@ -92,11 +92,11 @@ if [ ! -f /data/celeritas/Libs/hiredis_installed.txt ]; then
 fi
 
 #! 编译mongo库
-if [ ! -f /data/celeritas/deps/mongo-cxx-driver_installed.txt ]; then
+if [ ! -f ${DEPS_DIR}/mongo-cxx-driver_installed.txt ]; then
 
-    cd /data/celeritas/deps/
+    cd ${DEPS_DIR}/
 	
-	if [ ! -f /data/celeritas/deps/mongo-cxx-driver_clone.txt ]; then
+	if [ ! -f ${DEPS_DIR}/mongo-cxx-driver_clone.txt ]; then
 	
 		rm -rf mongo-cxx-driver
 
@@ -107,28 +107,28 @@ if [ ! -f /data/celeritas/deps/mongo-cxx-driver_installed.txt ]; then
 			cd mongo-cxx-driver
             git checkout releases/v4.1
 	 
-			touch /data/celeritas/deps/mongo-cxx-driver_clone.txt
+			touch ${DEPS_DIR}/mongo-cxx-driver_clone.txt
 	
 		fi
 	
 	fi 
 	
-	if [ -f /data/celeritas/deps/mongo-cxx-driver_clone.txt ]; then
+	if [ -f ${DEPS_DIR}/mongo-cxx-driver_clone.txt ]; then
 	
-		cd /data/celeritas/deps/mongo-cxx-driver
+		cd ${DEPS_DIR}/mongo-cxx-driver
 	
 		mkdir -p build
 		cd build
 
-		cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/data/celeritas/deps/mongo
+		cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${DEPS_DIR}/mongo
 		make
 		
-		rm -rf /data/celeritas/deps/mongo
+		rm -rf ${DEPS_DIR}/mongo
 		make install
 	 
 		if [ $? -eq 0 ]; then 
 	 
-			touch /data/celeritas/deps/mongo-cxx-driver_installed.txt
+			touch ${DEPS_DIR}/mongo-cxx-driver_installed.txt
 	
 		fi
 	
@@ -137,11 +137,11 @@ if [ ! -f /data/celeritas/deps/mongo-cxx-driver_installed.txt ]; then
 fi
 
 #! 编译protobuf库
-if [ ! -f /data/celeritas/deps/protobuf_installed.txt ]; then
+if [ ! -f ${DEPS_DIR}/protobuf_installed.txt ]; then
 
-    cd /data/celeritas/deps/
+    cd ${DEPS_DIR}/
 
-	if [ ! -f /data/celeritas/deps/protobuf_installed_clone.txt ]; then
+	if [ ! -f ${DEPS_DIR}/protobuf_installed_clone.txt ]; then
 
 		rm -rf protobuf_src
 
@@ -154,28 +154,28 @@ if [ ! -f /data/celeritas/deps/protobuf_installed.txt ]; then
 			git submodule update --init --recursive
             cd ..
 
-			touch /data/celeritas/deps/protobuf_installed_clone.txt
+			touch ${DEPS_DIR}/protobuf_installed_clone.txt
 
 		fi
 
 	fi
 
-	if [ -f /data/celeritas/deps/protobuf_installed_clone.txt ]; then
+	if [ -f ${DEPS_DIR}/protobuf_installed_clone.txt ]; then
 
-		cd /data/celeritas/deps/protobuf_src
+		cd ${DEPS_DIR}/protobuf_src
 
 		mkdir -p build
 		cd build
 
-		cmake .. -DCMAKE_BUILD_TYPE=Release -Dprotobuf_BUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=/data/celeritas/deps/protobuf
+		cmake .. -DCMAKE_BUILD_TYPE=Release -Dprotobuf_BUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=${DEPS_DIR}/protobuf
 		make
 
-		rm -rf /data/celeritas/deps/protobuf
+		rm -rf ${DEPS_DIR}/protobuf
 		make install
 
 		if [ $? -eq 0 ]; then
 
-			touch /data/celeritas/deps/protobuf_installed.txt
+			touch ${DEPS_DIR}/protobuf_installed.txt
 
 		fi
 
