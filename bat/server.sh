bf115ac03f4bc5997c022b65e319acfab629cd5c
彭武阳 <94458936@qq.com>
2025-10-21 15:16:57 +0800

    + daemon修改

--- bat/server.sh
+++ bat/server.sh
@@ -14,16 +14,16 @@
 # 注意：冒号前是用于命令行的“唯一名称”
 # ----------------------------------------------------
 declare -A servers=(
-    ["service registry"]="./service_registry_server:service_registry.pid" 
-    ["auth"]="./auth_server:auth.pid"  
-    ["payment"]="./payment_server:payment.pid"  
-    ["admin"]="./admin_server:admin.pid"  
-    ["chat"]="./chat_server:chat.pid"  
-    ["gateway"]="./gateway_server:gateway.pid"  
-    ["player"]="./player_server:player.pid"  
-    ["logic"]="./logic_server:logic.pid"  
-    ["game"]="./game_server:game.pid"  
-    ["cross"]="./cross_server:cross.pid"  
+    ["service registry"]="/data/celeritas/bin/debug/service_registry_server:service_registry.pid"
+    ["auth"]="/data/celeritas/bin/debug/auth_server:auth.pid"
+    ["payment"]="/data/celeritas/bin/debug/payment_server:payment.pid"
+    ["admin"]="/data/celeritas/bin/debug/admin_server:admin.pid"
+    ["chat"]="/data/celeritas/bin/debug/chat_server:chat.pid"
+    ["gateway"]="/data/celeritas/bin/debug/gateway_server:gateway.pid"
+    ["player"]="/data/celeritas/bin/debug/player_server:player.pid"
+    ["logic"]="/data/celeritas/bin/debug/logic_server:logic.pid"
+    ["game"]="/data/celeritas/bin/debug/game_server:game.pid"
+    ["cross"]="/data/celeritas/bin/debug/cross_server:cross.pid"
 )
 # ----------------------------------------------------
 
