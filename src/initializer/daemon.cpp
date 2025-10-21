6de05dcb27a9c330425edea442b41079703c4802
彭武阳 <94458936@qq.com>
2025-10-21 14:55:29 +0800

    + daemon修改

--- src/initializer/daemon.cpp
+++ src/initializer/daemon.cpp
@@ -3,16 +3,12 @@
 #include <boost/filesystem/operations.hpp>
 #include <unistd.h>
 #include <filesystem>
-#include <process.h>
 #include <fstream>
-#include <iostream>
 
 celeritas::daemon::daemon(const std::string_view& server_type)
     : server_type_{ server_type }, pid_file_name_{ (boost::filesystem::current_path() / (server_type_ + ".txt")).string() }
 {
-    const auto pid = std::filesystem::path::preferred_separator == '\\' ? _getpid() : getpid();
-
-    std::cout << pid_file_name_ << '\n';
+    const auto pid = getpid();
 
     std::ofstream process_id{ pid_file_name_ };
     process_id << pid << std::endl;
