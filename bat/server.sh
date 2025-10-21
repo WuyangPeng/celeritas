5e0dd47ee9274fb8266104e0741deaeb8818d155
彭武阳 <94458936@qq.com>
2025-10-21 16:19:27 +0800

    + daemon修改

--- bat/server.sh
+++ bat/server.sh
@@ -160,7 +160,7 @@ start_server() {
     # 3. 检查 PID 文件是否成功创建
     if [ $success -eq 1 ]; then
         local pid=$(cat "$pid_file")
-        echo "✅ 成功启动 $name。pid: pid"
+        echo "✅ 成功启动 $name。pid: $pid"
     else
         echo "❌ 启动失败！$name 未创建 $pid_file。请检查程序路径和权限。"
     fi
