49fbf51a7ebf65007a0562015865ed0e6d039093
彭武阳 <94458936@qq.com>
2025-10-24 17:31:10 +0800

    + 随机数

--- src/common/random_helper.cpp
+++ src/common/random_helper.cpp
@@ -2,7 +2,52 @@
 
 int celeritas::random_helper::get_random_int(const int end)
 {
-    std::uniform_int_distribution<> distribution{ 0, end - 1 };
+    std::uniform_int_distribution distribution{ 0, end - 1 };
+
+    return distribution(get_engine());
+}
+
+int celeritas::random_helper::get_random_int(const int begin, const int end)
+{
+    std::uniform_int_distribution distribution{ begin, end - 1 };
+
+    return distribution(get_engine());
+}
+
+float celeritas::random_helper::get_random_float()
+{
+    return get_random_float(0.0f, 1.0f);
+}
+
+float celeritas::random_helper::get_random_float(const float begin, const float end)
+{
+    std::uniform_real_distribution distribution{ begin, end };
+
+    return distribution(get_engine());
+}
+
+double celeritas::random_helper::get_random_double()
+{
+    return get_random_double(0.0, 1.0);
+}
+
+double celeritas::random_helper::get_random_double(const double begin, const double end)
+{
+    std::uniform_real_distribution distribution{ begin, end };
+
+    return distribution(get_engine());
+}
+
+bool celeritas::random_helper::get_random_bool(const double p)
+{
+    std::bernoulli_distribution distribution{ p };
+
+    return distribution(get_engine());
+}
+
+double celeritas::random_helper::get_random_normal(const double mean, const double stddev)
+{
+    std::normal_distribution distribution{ mean, stddev };
 
     return distribution(get_engine());
 }
--- src/common/random_helper.h
+++ src/common/random_helper.h
@@ -9,7 +9,31 @@ namespace celeritas
     public:
         using class_type = random_helper;
 
+        // 生成 [0, end) 范围内的 int
         [[nodiscard]] static int get_random_int(int end);
+
+        // 新增: 生成 [begin, end) 范围内的 int
+        [[nodiscard]] static int get_random_int(int begin, int end);
+
+        // 新增: 生成 [0.0f, 1.0f) 范围内的 float
+        [[nodiscard]] static float get_random_float();
+
+        // 新增: 生成 [begin, end) 范围内的 float
+        [[nodiscard]] static float get_random_float(float begin, float end);
+
+        // 新增: 生成 [0.0, 1.0) 范围内的 double
+        [[nodiscard]] static double get_random_double();
+
+        // 新增: 生成 [begin, end) 范围内的 double
+        [[nodiscard]] static double get_random_double(double begin, double end);
+
+        // 新增: 生成布尔值，p 是返回 true 的概率 (默认为 0.5)
+        [[nodiscard]] static bool get_random_bool(double p = 0.5);
+
+        // 新增: 生成服从正态分布的 double
+        // mean: 均值 (默认为 0.0)
+        // stddev: 标准差 (默认为 1.0)
+        [[nodiscard]] static double get_random_normal(double mean = 0.0, double stddev = 1.0);
 
     private:
         [[nodiscard]] static std::mt19937& get_engine();
