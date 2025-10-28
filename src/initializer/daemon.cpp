19b707f19bede8f7eade12d2d14b0e100038d5bc
彭武阳 <94458936@qq.com>
2025-10-28 12:41:36 +0800

    + 静态库分层

--- src/initializer/daemon.cpp
+++ src/initializer/daemon.cpp
@@ -1,14 +1,26 @@
 ﻿#include "daemon.h"
 
 #include <boost/filesystem/operations.hpp>
-#include <unistd.h>
+
 #include <filesystem>
 #include <fstream>
+
+#ifdef BOOST_MSVC
+
+#include <process.h>
+#define GET_PID _getpid()
+
+#else // !BOOST_MSVC
+
+#include <unistd.h>
+#define GET_PID getpid()
+
+#endif // BOOST_MSVC
 
 celeritas::daemon::daemon(const std::string_view server_type)
     : server_type_{ server_type }, pid_file_name_{ (boost::filesystem::current_path() / (server_type_ + ".pid")).string() }
 {
-    const auto pid = getpid();
+    const auto pid = GET_PID;
 
     std::ofstream process_id{ pid_file_name_ };
     process_id << pid << std::endl;
