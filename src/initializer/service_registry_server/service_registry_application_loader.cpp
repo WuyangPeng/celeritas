d6d7866b392cf53b6bc7a55f2eb135c1c21efe10
彭武阳 <94458936@qq.com>
2025-10-28 13:35:33 +0800

    + 通过编译

--- src/initializer/service_registry_server/service_registry_application_loader.cpp
+++ src/initializer/service_registry_server/service_registry_application_loader.cpp
@@ -2,6 +2,7 @@
 #include "service_registry/message_handler/discover_request_message_handler.h"
 #include "service_registry/message_handler/register_request_message_handler.h"
 #include "service_registry/message_handler/service_registry_request_message_handler.h"
+#include "message/concrete_message_handler.tpp"
 
 celeritas::service_registry_application_loader::service_registry_application_loader(app_config_shared_ptr app_config)
     : base_type{ std::move(app_config) }
