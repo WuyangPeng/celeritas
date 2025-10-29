fa01ecc8be7309e853f595e1d8b7c73a2706b425
彭武阳 <94458936@qq.com>
2025-10-29 15:46:32 +0800

    + 异常类

--- src/server/game_error_type.cpp
+++ src/server/game_error_type.cpp
@@ -1,19 +1,19 @@
 ﻿#include "game_error_type.h"
 
-std::string celeritas::get_game_error_description(game_error_type game_error_type)
-{
-    switch (game_error_type)
-    {
-        case game_error_type::Unknown:
-        {
-            return "Unknown error";
-        }
+#include <map>
 
-        case game_error_type::Success:
-        {
-            return "Success";
-        }
+std::string celeritas::get_game_error_description(const game_error_type game_error)
+{
+    using game_error_container_type = std::map<game_error_type, std::string>;
+
+    static game_error_container_type container{ { game_error_type::unknown, "unknown error." },
+                                                { game_error_type::success, "success." }, };
+
+    if (const auto iter = container.find(game_error);
+        iter != container.cend())
+    {
+        return iter->second;
     }
 
-    return "Unknown error";
+    return "error undefined.";
 }
--- src/server/game_error_type.h
+++ src/server/game_error_type.h
@@ -6,9 +6,9 @@ namespace celeritas
 {
     enum class game_error_type
     {
-        Unknown = 0,
-        Success = 1,
+        unknown = 0,
+        success = 1,
     };
 
-    [[nodiscard]] std::string get_game_error_description(game_error_type game_error_type);
+    [[nodiscard]] std::string get_game_error_description(game_error_type game_error);
 }
