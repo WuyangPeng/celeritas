272857f83b19ea6a431c01e1bf87af77215a5bb6
彭曄澤 <94458936@qq.com>
2025-11-13 20:54:40 +0800

    + proto 自动化生成

--- src/initializer/gateway_server/gateway_application_loader.cpp
+++ src/initializer/gateway_server/gateway_application_loader.cpp
@@ -5,14 +5,17 @@
 #include "message/concrete_message_handler.tpp"
 
 celeritas::gateway_application_loader::gateway_application_loader(app_config_shared_ptr app_config)
-    : base_type{std::move(app_config)} {
+    : base_type{ std::move(app_config) }
+{
 }
 
-void celeritas::gateway_application_loader::service_initialize_application() {
+void celeritas::gateway_application_loader::service_initialize_application()
+{
     register_message_handler();
 }
 
-void celeritas::gateway_application_loader::register_message_handler() {
+void celeritas::gateway_application_loader::register_message_handler()
+{
     register_handler(std::make_shared<service_registry_response_message_handler>());
     register_handler(std::make_shared<register_response_message_handler>());
     register_handler(std::make_shared<discover_response_message_handler>());
