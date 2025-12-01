c4e3829c03a7e86886f58872e2848d8446aa0aa1
彭曄澤 <94458936@qq.com>
2025-12-01 11:38:13 +0800

    + auth模块整理

--- src/auth/detail/auth_internal_fwd.h
+++ src/auth/detail/auth_internal_fwd.h
@@ -6,12 +6,16 @@ namespace celeritas
 {
     class auth_parameter;
     class guest_login_parameter;
+
     class phone_parameter;
     class send_sms_parameter;
     class phone_operation_parameter;
     class phone_login_parameter;
     class phone_bind_parameter;
+
     class email_parameter;
+    class send_email_parameter;
+    class email_operation_parameter;
 
     class send_sms_to_providers;
     class tencent_cloud_sms;
