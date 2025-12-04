b041c8870bb28a02ef803d3dc37599ad25a48894
彭曄澤 <94458936@qq.com>
2025-12-04 16:55:04 +0800

    + 服务器列表相关类

--- src/auth/login_servers_response.h
+++ src/auth/login_servers_response.h
@@ -24,8 +24,24 @@ namespace celeritas
 
         login_servers_response(bass_type http_response, container login_server_info);
 
+        [[nodiscard]] container get_login_server_info() const;
+
+        [[nodiscard]] virtual std::string to_json_string() const override;
+
+        [[nodiscard]] static login_servers_response from_json_string(const std::string& json_string);
+
+        [[nodiscard]] static login_servers_response tag_invoke(const json_value& value);
+
+        static constexpr std::string_view login_server_info_description = "login_server_info";
+
     private:
         container login_server_info_;
     };
+
+    using login_servers_response_tag = boost::json::value_to_tag<login_servers_response>;
+
+    [[nodiscard]] login_servers_response tag_invoke(login_servers_response_tag, const login_servers_response::json_value& value);
+
+    void tag_invoke(boost::json::value_from_tag tag, login_servers_response::json_value& value, const login_servers_response& login_servers_response);
 }
 
