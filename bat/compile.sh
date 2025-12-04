29037daf87c1d0b7ad8e05d885ffb1352c610105
彭曄恩 <94458936@qq.com>
2025-12-04 22:46:21 +0800

    + 运行测试

--- bat/compile.sh
+++ bat/compile.sh
@@ -9,10 +9,12 @@ cd build-debug
 cmake ..
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make
+make test
 cmake --install . 
 
 cd ../build-release
 cmake ..
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
+make test
 cmake --install . 
