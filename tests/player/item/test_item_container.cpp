62435676246b1386648d0484dda507636e6d93f0
彭曄澤 <94458936@qq.com>
2025-12-22 13:44:28 +0800

    + 道具模块单元测试

---
+++ tests/player/item/test_item_container.cpp
@@ -0,0 +1,31 @@
+﻿#include "player/item/item_container.h"
+#include "player/item/item_info.h"
+
+#include <boost/test/unit_test.hpp>
+
+using namespace celeritas;
+
+BOOST_AUTO_TEST_SUITE(item_container_test_suite)
+
+    BOOST_AUTO_TEST_CASE(test_item_container_add_and_iterate)
+    {
+        item_container container{};
+
+        container.add_item_info(1001, 10);
+        container.add_item_info(1002, 20);
+
+        auto iter = container.begin();
+        BOOST_CHECK(iter != container.end());
+        BOOST_CHECK_EQUAL(iter->get_template_id(), 1001);
+        BOOST_CHECK_EQUAL(iter->get_count(), 10);
+
+        ++iter;
+        BOOST_CHECK(iter != container.end());
+        BOOST_CHECK_EQUAL(iter->get_template_id(), 1002);
+        BOOST_CHECK_EQUAL(iter->get_count(), 20);
+
+        ++iter;
+        BOOST_CHECK(iter == container.end());
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ tests/player/item/test_item_info.cpp
@@ -0,0 +1,20 @@
+﻿#include "player/item/item_container.h"
+#include "player/item/item_info.h"
+
+#include <boost/test/unit_test.hpp>
+
+using namespace celeritas;
+
+BOOST_AUTO_TEST_SUITE(item_info_test_suite)
+
+    BOOST_AUTO_TEST_CASE(test_item_info)
+    {
+        constexpr auto template_id = 1001;
+        constexpr auto count = 50LL;
+        const item_info info{ template_id, count };
+
+        BOOST_CHECK_EQUAL(info.get_template_id(), template_id);
+        BOOST_CHECK_EQUAL(info.get_count(), count);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
