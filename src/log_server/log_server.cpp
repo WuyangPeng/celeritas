598290b0097466485b5687469afd807c51accbfe
彭曄澤 <94458936@qq.com>
2025-12-31 12:59:16 +0800

    + common模块代码重构

--- src/log_server/log_server.cpp
+++ src/log_server/log_server.cpp
@@ -13,7 +13,7 @@ int main(const int argc, char** argv)
 {
     try
     {
-        const celeritas::log_server server{};
+        celeritas::log_server server{};
 
         server.run(argc, argv);
     }
