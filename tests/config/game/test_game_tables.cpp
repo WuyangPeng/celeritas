263f0b4b1d0f3eb5d898de8e8e2965c48a32b5f7
彭曄澤 <94458936@qq.com>
2026-01-04 16:39:41 +0800

    + 移动代码目录

--- tests/config/game/test_game_tables.cpp
+++ tests/config/game/test_game_tables.cpp
@@ -11,6 +11,46 @@ namespace
 {
     constexpr auto random_count = 10;
 
+    void write_surname_config(luban::ByteBuf& out_buf)
+    {
+        // 写入大小 2
+        out_buf.writeSize(2);
+
+        // surname 1: id=1, name="Zhao", weight=10
+        out_buf.writeInt(1);
+        out_buf.writeString("Zhao");
+        out_buf.writeInt(10);
+
+        // surname 2: id=2, name="Qian", weight=20
+        out_buf.writeInt(2);
+        out_buf.writeString("Qian");
+        out_buf.writeInt(20);
+    }
+
+    void write_name_config(luban::ByteBuf& out_buf)
+    {
+        // 写入大小 3
+        out_buf.writeSize(3);
+
+        // name 1: id=1, name="MaleName", sexType=male(1), weight=10
+        out_buf.writeInt(1);
+        out_buf.writeString("MaleName");
+        out_buf.writeInt(1);
+        out_buf.writeInt(10);
+
+        // name 2: id=2, name="FemaleName", sexType=female(2), weight=10
+        out_buf.writeInt(2);
+        out_buf.writeString("FemaleName");
+        out_buf.writeInt(2);
+        out_buf.writeInt(10);
+
+        // name 3: id=3, name="AnyName", sexType=none(0), weight=10
+        out_buf.writeInt(3);
+        out_buf.writeString("AnyName");
+        out_buf.writeInt(0);
+        out_buf.writeInt(10);
+    }
+
     [[nodiscard]] std::shared_ptr<const celeritas::config::tables> create_test_tables()
     {
         // 创建 tables 对象并加载
@@ -22,41 +62,11 @@ namespace
 
             if (name == "surname_config_container")
             {
-                // 写入大小 2
-                out_buf.writeSize(2);
-
-                // surname 1: id=1, name="Zhao", weight=10
-                out_buf.writeInt(1);
-                out_buf.writeString("Zhao");
-                out_buf.writeInt(10);
-
-                // surname 2: id=2, name="Qian", weight=20
-                out_buf.writeInt(2);
-                out_buf.writeString("Qian");
-                out_buf.writeInt(20);
+                write_surname_config(out_buf);
             }
             else if (name == "name_config_container")
             {
-                // 写入大小 3
-                out_buf.writeSize(3);
-
-                // name 1: id=1, name="MaleName", sexType=male(1), weight=10
-                out_buf.writeInt(1);
-                out_buf.writeString("MaleName");
-                out_buf.writeInt(1);
-                out_buf.writeInt(10);
-
-                // name 2: id=2, name="FemaleName", sexType=female(2), weight=10
-                out_buf.writeInt(2);
-                out_buf.writeString("FemaleName");
-                out_buf.writeInt(2);
-                out_buf.writeInt(10);
-
-                // name 3: id=3, name="AnyName", sexType=none(0), weight=10
-                out_buf.writeInt(3);
-                out_buf.writeString("AnyName");
-                out_buf.writeInt(0);
-                out_buf.writeInt(10);
+                write_name_config(out_buf);
             }
             else
             {
