3761e07f48e8c2bb6b418de638096abc2887a442
彭曄澤 <94458936@qq.com>
2026-01-31 10:16:51 +0800

    +测试模块

--- src/handler/player/debug_message_handler.cpp
+++ src/handler/player/debug_message_handler.cpp
@@ -1,6 +1,11 @@
 ﻿#include "debug_message_handler.h"
+#include "common/common_constant.h"
+#include "handler/player/manual/player_concrete_message_handler.tpp"
+#include "player/debug/debug_base.h"
 
 bool celeritas::debug_message_handler::handle_concrete(const protobuf_handle_parameter_shared_ptr& handle_parameter, const message_type& current_message, const message_registry_weak_ptr& message_registry)
 {
+    player_co_spawn_response<debug_base>(handle_parameter, current_message, handler_channel, "debug error:");
+
     return true;
 }
