8052e75c038d9018c150af3ea357f688a9c508e3
彭曄澤 <94458936@qq.com>
2026-02-05 16:50:58 +0800

    + 测试

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,12 +2,7 @@
 
 #include <boost/test/unit_test.hpp>
 
-#include <any>
-
 BOOST_AUTO_TEST_CASE(empty_test)
 {
     BOOST_CHECK(true);
-    std::any a{1LL};
-    auto b = std::any_cast<int64_t>(a);
-    BOOST_CHECK(b == 1LL);
 }
